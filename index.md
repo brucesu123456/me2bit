147d9174d14a0113757748b0701666d1e4d63712
brucesu123456 <brucesu123456@gmail.com>
2025-05-11 14:59:10 +0800

    update web info

--- index.md
+++ index.md
@@ -2,13 +2,13 @@
 layout: home
 sidebar: false
 
-title: Nólëbase
-titleTemplate: 记录回忆，知识和畅想的地方
+title: me2Bit
+titleTemplate: 硅基生存指南
 
 hero:
-  name: g~Nj$3J2^
-  text: 记录回忆，知识和畅想的地方
-  tagline: 以 Nólëbase 为名，读作 nole-base，取自意为「知识」的昆雅语 nólë 和意为「基础」的英文 base，即「知识库」
+  name: me2bit
+  text: 硅生存指南
+  tagline: 硅基生存指南。。。。。。 
   image:
     src: /logo.svg
     alt: Vitest
@@ -18,10 +18,10 @@ hero:
       link: /笔记/index
     - theme: alt
       text: 加入 Discord 服务器
-      link: https://discord.gg/XuNFDcDZGj
+      link: https://discord.gg/brucesu123456
     - theme: alt
       text: GitHub 上浏览
-      link: https://github.com/nolebase/nolebase
+      link: https://github.com/brucesu123456/me2bit
 
 features:
   - title: 多样的主题和内容
--- metadata/index.ts
+++ metadata/index.ts
@@ -1,17 +1,17 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'me2Bit'
+export const siteShortName = 'me2Bit'
+export const siteDescription = '硅基生存指南'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/brucesu123456/me2bit'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://discord.gg/brucesu123456'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'me2bit.ayaka.io'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
